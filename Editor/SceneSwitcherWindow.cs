019d464c5bd449ac1c9fffe671c12b87c81252e4
Jason Skillman <jason_skillman@outlook.com>
2022-12-03 11:08:17 -0500

    Update namespace

--- Editor/SceneSwitcherWindow.cs
+++ Editor/SceneSwitcherWindow.cs
@@ -1,107 +1,105 @@
-using UnityEditor;
-using UnityEditor.SceneManagement;
-using UnityEditorInternal;
-using UnityEngine;
+namespace JasonSkillman.SceneSwitcher.Editor {
+	using UnityEditor;
+	using UnityEditor.SceneManagement;
+	using UnityEditorInternal;
+	using UnityEngine;
 
-public class SceneSwitcherWindow : EditorWindow {
-	
-	private struct SceneAsset {
-		public string GUID { get; private set; }
-		public string Path => AssetDatabase.GUIDToAssetPath(GUID);
-		public string Name {
-			get {
-				//Remove the file's path
-				int index = 0;
-				for(var i = 0; i < Path.Length; i++) {
-					char c = Path[i];
-					if(c.Equals('/'))
-						index = i;
+	public class SceneSwitcherWindow : EditorWindow {
+
+		private struct SceneAsset {
+			public string GUID { get; private set; }
+			public string Path => AssetDatabase.GUIDToAssetPath(GUID);
+			public string Name {
+				get {
+					//Remove the file's path
+					int index = 0;
+					for(var i = 0; i < Path.Length; i++) {
+						char c = Path[i];
+						if(c.Equals('/'))
+							index = i;
+					}
+
+					int extLength = 6; //.unity
+
+					return Path.Substring(index + 1, Path.Length - index - 1 - extLength);
 				}
+			}
 
-				int extLength = 6;	//.unity
+			public SceneAsset(string guid) { GUID = guid; }
+		}
 
-				return Path.Substring(index + 1, Path.Length - index - 1 - extLength);
+		private ReorderableList reorderableList;
+		private SceneAsset[] sceneObjs;
+		private string filter;
+
+		[MenuItem("Window/Scene Switcher")]
+		private static void OpenWindow() {
+			SceneSwitcherWindow window = (SceneSwitcherWindow) GetWindow(typeof(SceneSwitcherWindow), false, "Scene Switcher");
+			window.Show();
+		}
+
+		private void OnGUI() {
+			//Draw search bar
+			filter = EditorGUILayout.TextField("Search", filter);
+
+			UpdateList();
+
+			reorderableList.DoList(new Rect(0.0f, 30.0f, position.width, position.height));
+		}
+
+		private void CacheScenes() {
+			string[] sceneGUIDs = AssetDatabase.FindAssets("t: " + nameof(UnityEngine.SceneManagement.Scene) + " " + filter);
+			sceneObjs = new SceneAsset[sceneGUIDs.Length];
+
+			for(int i = 0; i < sceneGUIDs.Length; i++) {
+				SceneAsset sceneAsset = new SceneAsset(sceneGUIDs[i]);
+				sceneObjs[i] = sceneAsset;
 			}
 		}
 
-		public SceneAsset(string guid) {
-			GUID = guid;
+		private void CreateReorderableList() {
+			reorderableList = new ReorderableList(sceneObjs, typeof(SceneAsset), false, false, false, false);
+			reorderableList.drawElementCallback = DrawElementCallback;
+			reorderableList.elementHeight = 24.0f;
 		}
-	}
 
-	private ReorderableList reorderableList;
-	private SceneAsset[] sceneObjs;
-	private string filter;
-	
-	[MenuItem("Window/Scene Switcher")]
-	private static void OpenWindow() {
-		SceneSwitcherWindow window = (SceneSwitcherWindow)GetWindow(typeof(SceneSwitcherWindow), false, "Scene Switcher");
-		window.Show();
-	}
-
-	private void OnGUI() {
-		//Draw search bar
-		filter = EditorGUILayout.TextField("Search", filter);
-
-		UpdateList();
-
-		reorderableList.DoList(new Rect(0.0f, 30.0f, position.width, position.height));
-	}
-
-	private void CacheScenes() {
-		string[] sceneGUIDs = AssetDatabase.FindAssets("t: " + nameof(UnityEngine.SceneManagement.Scene) + " " + filter);
-		sceneObjs = new SceneAsset[sceneGUIDs.Length];
-		
-		for(int i = 0; i < sceneGUIDs.Length; i++) {
-			SceneAsset sceneAsset = new SceneAsset(sceneGUIDs[i]);
-			sceneObjs[i] = sceneAsset;
+		private void UpdateList() {
+			CacheScenes();
+			CreateReorderableList();
 		}
-	}
-	
-	private void CreateReorderableList() {
-		reorderableList = new ReorderableList(sceneObjs, typeof(SceneAsset), false, false, false, false);
-		reorderableList.drawElementCallback = DrawElementCallback;
-		reorderableList.elementHeight = 24.0f;
-	}
 
-	private void UpdateList() {
-		CacheScenes();
-		CreateReorderableList();
-	}
-	
-	private void DrawElementCallback(Rect rect, int index, bool isactive, bool isfocused) {
-		Rect currentRect = rect;
+		private void DrawElementCallback(Rect rect, int index, bool isactive, bool isfocused) {
+			Rect currentRect = rect;
 
-		//Draw single load button
-		{
-			currentRect.width = 20.0f;
-			currentRect.height = 18.0f;
-			currentRect.y += (rect.height - currentRect.height) / 2;
-			if(GUI.Button(currentRect, new GUIContent("S", "Opens the scene"))) {
-				LoadScene(index);
+			//Draw single load button
+			{
+				currentRect.width = 20.0f;
+				currentRect.height = 18.0f;
+				currentRect.y += (rect.height - currentRect.height) / 2;
+				if(GUI.Button(currentRect, new GUIContent("S", "Opens the scene"))) {
+					LoadScene(index);
+				}
+			}
+
+			//Draw additive load button
+			{
+				currentRect.x += 25;
+				currentRect.width = 20.0f;
+				currentRect.height = 18.0f;
+				if(GUI.Button(currentRect, new GUIContent("A", "Opens the scene additively"))) {
+					LoadScene(index, OpenSceneMode.Additive);
+				}
+			}
+
+			//Draw label
+			{
+				currentRect.x += 40.0f;
+				currentRect.width = 200.0f;
+				GUI.Label(currentRect, new GUIContent(sceneObjs[index].Name, sceneObjs[index].Path));
 			}
 		}
-		
-		//Draw additive load button
-		{
-			currentRect.x += 25;
-			currentRect.width = 20.0f;
-			currentRect.height = 18.0f;
-			if(GUI.Button(currentRect, new GUIContent("A", "Opens the scene additively"))) {
-				LoadScene(index, OpenSceneMode.Additive);
-			}
-		}
-		
-		//Draw label
-		{
-			currentRect.x += 40.0f;
-			currentRect.width = 200.0f;
-			GUI.Label(currentRect, new GUIContent(sceneObjs[index].Name, sceneObjs[index].Path));
-		}
-	}
 
-	private void LoadScene(int index, OpenSceneMode mode = OpenSceneMode.Single) {
-		EditorSceneManager.OpenScene(sceneObjs[index].Path, mode);
-	}
+		private void LoadScene(int index, OpenSceneMode mode = OpenSceneMode.Single) { EditorSceneManager.OpenScene(sceneObjs[index].Path, mode); }
 
+	}
 }
